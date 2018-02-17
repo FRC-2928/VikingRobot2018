4d72b80edeec35aa9f5dfb0b7504c724cfb7f297
Sam Burns <samburns157@gmail.com>
2018-02-17 13:52:08 -0800

    More auto work

--- src/main/java/org/usfirst/frc/team2928/Autonomous/SmartAuto.java
+++ src/main/java/org/usfirst/frc/team2928/Autonomous/SmartAuto.java
@@ -5,16 +5,20 @@ import edu.wpi.first.wpilibj.command.CommandGroup;
 import org.usfirst.frc.team2928.Match;
 
 import java.text.FieldPosition;
+import java.util.ArrayList;
+import java.util.List;
 
 public class SmartAuto extends Command
 {
-
     public enum AutoTarget
     {
         SCALE,
         SWITCH,
         LINE
     }
+
+    private Match.Position startingPosition;
+    private AutoTarget target;
 
     @Override
     public boolean isFinished()
@@ -24,11 +28,26 @@ public class SmartAuto extends Command
 
     public void goForSwitch()
     {
+    }
+
+    public SmartAuto(Match.Position start, AutoTarget target)
+    {
+        this.startingPosition = start;
+        this.target = target;
+    }
+
+    private TransformableWaypoint[] generatePath(AutoTarget target)
+    {
 
     }
 
-    public SmartAuto()
+    private List<TransformableWaypoint> targetSwitch()
     {
+        List<TransformableWaypoint> path = new ArrayList<>();
 
+        if (startingPosition == Match.getInstance().nearSwitch || startingPosition == Match.Position.MIDDLE)
+        {
+            path.add(Waypoints.)
+        }
     }
 }
