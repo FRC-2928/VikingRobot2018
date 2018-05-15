709e8be63a748af58724967ee8c9861f76fc9886
Sam Burns <samburns157@gmail.com>
2018-05-14 20:03:55 -0700

    Tyr updates, TODO: set motor ports

--- src/main/java/org/usfirst/frc/team2928/OperatorInterface.java
+++ src/main/java/org/usfirst/frc/team2928/OperatorInterface.java
@@ -2,10 +2,12 @@ package org.usfirst.frc.team2928;
 
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.buttons.JoystickButton;
+import org.usfirst.frc.team2928.Command.Chassis.Shift;
 import org.usfirst.frc.team2928.Command.Shooter.RunIntake;
 import org.usfirst.frc.team2928.Command.Shooter.RunLifter;
 import org.usfirst.frc.team2928.Command.Shooter.SetTomahawk;
 import org.usfirst.frc.team2928.Command.Shooter.Shoot;
+import org.usfirst.frc.team2928.Subsystem.Chassis.Transmission;
 import org.usfirst.frc.team2928.Subsystem.Shooter.Tomahawk;
 
 public class OperatorInterface {
@@ -15,20 +17,23 @@ public class OperatorInterface {
 
     public static JoystickButton TomahawkUp = new JoystickButton(operatorConsole, 7);
     public static JoystickButton TomahawkDown = new JoystickButton(operatorConsole, 6);
-    public static JoystickButton Flipper = new JoystickButton(operatorConsole, 5);
+    public static JoystickButton Flipper = new JoystickButton(operatorConsole, 8);
     public static JoystickButton ShooterUp = new JoystickButton(operatorConsole, 10);
     public static JoystickButton ShooterDown = new JoystickButton(operatorConsole, 11);
     public static JoystickButton IntakeIn = new JoystickButton(operatorConsole, 3);
     public static JoystickButton IntakeOut = new JoystickButton(operatorConsole, 2);
+    public static JoystickButton Shifter = new JoystickButton(driveStick, 9);
 
     OperatorInterface() {
         TomahawkUp.whenPressed(new SetTomahawk(Tomahawk.TomahawkState.RAISE));
         TomahawkDown.whenPressed(new SetTomahawk(Tomahawk.TomahawkState.LOWER));
-        Flipper.whenPressed(new Shoot());
-        ShooterUp.whileHeld(new RunLifter(0.4));
-        ShooterDown.whileHeld(new RunLifter(-0.4));
+        Flipper.whileHeld(new Shoot());
+        ShooterUp.whileHeld(new RunLifter(0.35));
+        ShooterDown.whileHeld(new RunLifter(-0.35));
         IntakeIn.whileHeld(new RunIntake(-0.75));
         IntakeOut.whileHeld(new RunIntake(0.75));
+        Shifter.whenPressed(new Shift(Transmission.GearState.HIGH));
+        Shifter.whenReleased(new Shift(Transmission.GearState.LOW));
     }
 
     //We're assuming same drive setup as last year.
